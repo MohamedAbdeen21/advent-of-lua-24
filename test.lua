658f92aa7a889a58f7131ef9e439993860a9e6fe
Mohamed Abdeen <mohamedabden21@gmail.com>
2024-12-06 17:45:03 +0000

    suppress print function in tests

--- test.lua
+++ test.lua
@@ -4,7 +4,17 @@ local reset = "\27[0m" -- Reset to default color
 
 local function test(module)
 	local m = require(module)
-	if pcall(m.tests) then
+
+	-- suppress calls to print
+	local p = print
+	print = function() end
+
+	local test_success = pcall(m.tests)
+
+	-- restore print functionality
+	print = p
+
+	if test_success then
 		print(green .. "Tests for " .. module .. " passed!" .. reset)
 	else
 		print(red .. "Tests for " .. module .. " failed!" .. reset)
@@ -16,3 +26,4 @@ test("day2")
 test("day3")
 test("day4")
 test("day5")
+test("day6")
