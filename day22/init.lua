6be7f381b93c47baa0a98c6f30f66778c14c4d99
Mohamed Abdeen <mohamedabden21@gmail.com>
2024-12-22 12:52:59 +0000

    day 22 part 2

--- day22/init.lua
+++ day22/init.lua
@@ -2,19 +2,19 @@ local util = require("util")
 
 local M = {}
 
-local function calculate(number, cycles)
+local function calculate(number, cycles, numbers)
 	local result = 0
 
 	for _ = 1, cycles do
+		if numbers then
+			table.insert(numbers, number % 10)
+		end
 		result = number << 6
-		number = number ~ result
-		number = number % 16777216
+		number = number ~ result % 16777216
 		result = number >> 5
-		number = number ~ result
-		number = number % 16777216
+		number = number ~ result % 16777216
 		result = number << 11
-		number = number ~ result
-		number = number % 16777216
+		number = number ~ result % 16777216
 	end
 
 	return number
@@ -33,16 +33,46 @@ end
 
 function M.part2(lines)
 	lines = lines or util.lines_from("./day22/input.txt")
+
+	local all_prices = {}
+	local prices = {}
 	for _, line in ipairs(lines) do
-		print(line)
+		calculate(tonumber(line), 2000, prices)
+		table.insert(all_prices, prices)
+		prices = {}
 	end
-	return 0
+
+	local all_changes = {}
+	for _, number in ipairs(all_prices) do
+		local seen = {}
+		for i = 1, #number - 4 do
+			local change = number[i + 1] - number[i]
+				.. number[i + 2] - number[i + 1]
+				.. number[i + 3] - number[i + 2]
+				.. number[i + 4] - number[i + 3]
+
+			if not seen[change] then
+				seen[change] = true
+				all_changes[change] = (all_changes[change] or 0) + number[i + 4]
+			end
+		end
+	end
+
+	local maximum = 0
+	for _, count in pairs(all_changes) do
+		if count > maximum then
+			maximum = count
+		end
+	end
+
+	return maximum
 end
 
 function M.tests()
 	local input = util.lines_from("./day22/test.txt")
+	local input2 = util.lines_from("./day22/test2.txt")
 	util.run_test(M.part1, input, 37327623)
-	util.run_test(M.part2, input, 0)
+	util.run_test(M.part2, input2, 23)
 end
 
 return M
